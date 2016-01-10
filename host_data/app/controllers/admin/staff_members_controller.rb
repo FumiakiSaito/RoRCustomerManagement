22baee24080eb2e90937a3e82948563676b025ce
fumiaki <haratairani3000points@gmail.com>
2016-01-11 03:40:47 +0900

    職員一覧

--- host_data/app/controllers/admin/staff_members_controller.rb
+++ host_data/app/controllers/admin/staff_members_controller.rb
@@ -1,2 +1,6 @@
-class Admin::StaffMembersController < ApplicationController
+class Admin::StaffMembersController < Admin::Base
+  def index
+    # 姓、名の順に全レコード取得
+    @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
+  end
 end
